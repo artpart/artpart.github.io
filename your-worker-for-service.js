9ef8e5d6e01748c39ca89ec9788e3a6e32b3f642
Artur <ArturWeikum@gmail.com>
2019-01-23 13:21:46 +0100

    Update your-worker-for-service.js

--- your-worker-for-service.js
+++ your-worker-for-service.js
@@ -2,18 +2,18 @@ var filesToCatch = [
     '/',
     '/index.html',
     '/besuchsbericht_erstellen.html',
-    '/jquery-3.3.1.min.js',
+    '/scripts/jquery-3.3.1.min.js',
     '/scripts/main.js',
     '/styles/main.css'
 
 ];
 
-var cacheName = 'pwa-v1';
+//var cacheName = 'pwa-v1';
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
+        caches.open('pwa-v1').then(function (cache) {
             console.log('[Service Worker] Caching all: app shell and content');
             return cache.addAll(filesToCatch);
         })

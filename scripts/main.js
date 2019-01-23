bad03ed6c168cb752f53eded6ae5044cbae4a966
Artur <ArturWeikum@gmail.com>
2019-01-23 12:45:50 +0100

    Edit

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/your-worker-for-service.js');
-};
-
-$("#besuchsbericht_erstellen").click(function () {
-    alert("Danke!");
-});

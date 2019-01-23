ebfaac87b37a35e13639052e24bfe73a8edbf0a0
Artur <ArturWeikum@gmail.com>
2019-01-23 17:34:59 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,43 @@
-$("#besuchsbericht_erstellen").click(function () {
-    alert("Danke!");
-});
-
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/your-worker-for-service.js').then(function () {
-        console.log('Service Worker Registered');
+if (document.readyState === 'complete') {
+    $("#besuchsbericht_erstellen").click(function () {
+        alert("Danke!");
     });
-};
+
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/your-worker-for-service.js').then(function () {
+            console.log('Service Worker Registered');
+        });
+    };
 
 
 
-let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+    let deferredPrompt;
+    const addBtn = document.querySelector('.add-button');
+    addBtn.style.display = 'none';
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
+    window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        // Update UI to notify the user they can add to home screen
+        addBtn.style.display = 'block';
 
-    addBtn.addEventListener('click', (e) => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
+        addBtn.addEventListener('click', (e) => {
+            // hide our user interface that shows our A2HS button
+            addBtn.style.display = 'none';
+            // Show the prompt
+            deferredPrompt.prompt();
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+            });
         });
     });
-});
+}
